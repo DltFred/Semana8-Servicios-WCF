c3843dc8d2d068a51502edf6d1225712cd5239ad
FredDlt <fredy.delatorre.frd@gmail.com>
2020-10-26 21:16:43 -0500

    Agregar archivos de proyecto.

---
+++ WCFNegocios/IService1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFNegocios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract] public interface IService1
+    {
+        [OperationContract] List<Cliente> cliente();
+        [OperationContract] List<Pedido> pedidos();
+        [OperationContract] List<Pedido> pedido_cliente(String idcli);
+        [OperationContract] List<Pedido> pedido_anio(int y);
+
+    }
+
+    [DataContract] public class Pedido
+    {
+        [DataMember] public int idpedido { get; set; }
+        [DataMember] public DateTime fechapedido { get; set; }
+        [DataMember] public string idcliente { get; set; }
+        [DataMember] public string direccion { get; set; }
+        [DataMember] public string ciudad { get; set; }
+    }
+
+    [DataContract] public class Cliente
+    {
+        [DataMember] public string idcliente { get; set; }
+        [DataMember] public string nombrecia { get; set; }
+        [DataMember] public string direccion { get; set; }
+        [DataMember] public string telefono { get; set; }
+    }
+
+}
---
+++ WCFNegocios/Service1.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFNegocios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public List<Cliente> cliente()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pedido> pedidos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pedido> pedido_anio(int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pedido> pedido_cliente(string idcli)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
